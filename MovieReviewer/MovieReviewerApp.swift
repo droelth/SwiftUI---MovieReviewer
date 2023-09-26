bb01fcd1eb59f14128a83420522c0f45917d140a
Berkay <berkaysutlu94@gmail.com>
2023-09-26 14:33:01 +0300

    Initial Commit

---
+++ MovieReviewer/MovieReviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReviewerApp.swift
+//  MovieReviewer
+//
+//  Created by Berkay Sutlu on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieReviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
